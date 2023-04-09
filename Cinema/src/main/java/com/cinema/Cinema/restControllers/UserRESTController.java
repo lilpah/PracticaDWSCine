a478415f53860290e907cbc77c860925839cbda0
lilpah <95379856+lilpah@users.noreply.github.com>
2023-04-09 15:21:38 +0200

    modify user changed correctly

--- Cinema/src/main/java/com/cinema/Cinema/restControllers/UserRESTController.java
+++ Cinema/src/main/java/com/cinema/Cinema/restControllers/UserRESTController.java
@@ -16,17 +16,26 @@ public class UserRESTController {
 
     @GetMapping("/user/{id}")
     public User getUser(@PathVariable long id){
-        return userService.getUsers().get(id);
+        //return userService.getUsers().get(id);
+        return userService.getUser(id).get();
     }
 
     @PostMapping("/user/{id}")
     public User getUser2(@PathVariable long id){
-        return userService.getUsers().get(id);
+        //return userService.getUsers().get(id);
+        return userService.getUser(id).get();
     }
 
     @DeleteMapping("/deleteUser/{id}")
     public ResponseEntity<User> deleteUser(@PathVariable long id) {
-        if(userService.getUsers().get(id).getId() == -1){
+        /*if(userService.getUsers().get(id).getId() == -1){
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }else{
+            userService.deleteUser(id);
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
+         */
+        if(userService.getUser(id).get().getId() == -1){
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }else{
             userService.deleteUser(id);
@@ -51,23 +60,32 @@ public class UserRESTController {
     @GetMapping("/showAllUsers")
     @ResponseStatus(HttpStatus.OK)
     public String showAllUsers(){
+        //return userService.getUsers().toString();
         return userService.getUsers().toString();
     }
 
     @PostMapping("/showAllUsers")
     @ResponseStatus(HttpStatus.OK)
     public String showAllUsers2(){
+        //return userService.getUsers().toString();
         return userService.getUsers().toString();
     }
 
     @PutMapping("updateUser/{id}")
     public ResponseEntity<User> showAllUsers(@PathVariable long id, @RequestBody User updateUser){
-      //  User user = new User(name, surname, pass, email);
-        if(userService.getUsers().get(id).getId() != -1) {
+       /* if(userService.getUsers().get(id).getId() != -1) {
             return new ResponseEntity<>(userService.getUsers().put(id, updateUser), HttpStatus.OK);
         }
         else{
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        */
+        if(userService.getUser(id).get().getId() == -1) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        else{
+            userService.modifyUser(id, updateUser);
+            return new ResponseEntity<>(HttpStatus.OK);
         }
 
     }
