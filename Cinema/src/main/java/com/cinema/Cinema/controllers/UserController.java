829ae73316a838da6af0ce7e131868646b66b442
ch0ve <naachovelazquez2003@gmail.com>
2023-04-11 08:12:45 +0200

    comit contact

--- Cinema/src/main/java/com/cinema/Cinema/controllers/UserController.java
+++ Cinema/src/main/java/com/cinema/Cinema/controllers/UserController.java
@@ -78,6 +78,9 @@ public class UserController {
 
     @GetMapping("/contact/formComment")
     public String formComment(@RequestParam String comment, Model model){
+        if(!(comment.contains("<script>"))){
+            return "reviews";
+        }
         model.addAttribute("comment", comment);
         return "contact";
     }
