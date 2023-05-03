77401229797a42ec8914083a2cbeed621607c5cf
lilpah <95379856+lilpah@users.noreply.github.com>
2023-05-04 00:02:40 +0200

    Modified user

--- Cinema/src/main/java/com/cinema/Cinema/controllers/UserController.java
+++ Cinema/src/main/java/com/cinema/Cinema/controllers/UserController.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
@@ -70,7 +71,7 @@ public class UserController {
         userService.deleteUser(id);
         return "userDeleted";
     }
-    @GetMapping("/addUsers")
+    @GetMapping("/register")
     public String addUser(Model model){
         model.addAttribute("users", userService.getUsers());
         return "addUsers";
@@ -80,27 +81,61 @@ public class UserController {
     public String formUser(Model model,  @RequestParam String pass, @RequestParam String name, @RequestParam String surname,@RequestParam String email){
         userService.addUser(new User(StringEscapeUtils.escapeHtml4(name),StringEscapeUtils.escapeHtml4(surname),StringEscapeUtils.escapeHtml4(passwordEncoder.encode(pass)),StringEscapeUtils.escapeHtml4(email),"USER"));
         model.addAttribute("user", StringEscapeUtils.escapeHtml4(name));
-        return "redirect:login";
+        return "redirect: login";
     }
 
-    @GetMapping("/user/{id}/modifyUser")
-    public String modifyUser(Model model, @PathVariable Long id){
-        model.addAttribute("name", userService.getUser(id).getName());
-        model.addAttribute("surname", userService.getUser(id).getSurname());
-        model.addAttribute("pass", userService.getUser(id).getPass());
-        model.addAttribute("email", userService.getUser(id).getEmail());
+
+
+
+ // no va la zorra
+   /* @PostMapping("/register")
+    public String addUser(Model model, @RequestParam("name") String name, @RequestParam("email") String surname, @RequestParam("email") String email, @RequestParam("password") String pass){
+       // userService.addUser(user);
+        userService.addUser(new User(StringEscapeUtils.escapeHtml4(name),StringEscapeUtils.escapeHtml4(surname),StringEscapeUtils.escapeHtml4(passwordEncoder.encode(pass)),StringEscapeUtils.escapeHtml4(email),"USER"));
+        model.addAttribute("user", StringEscapeUtils.escapeHtml4(name));
+        return "redirect:/login";
+    }
+
+    */
+
+
+
+
+
+    @GetMapping("/user/modifyUser")
+    public String modifyUser(Model model, HttpServletRequest request){
+        String name = request.getUserPrincipal().getName();
+        User user = userRepository.findByName(name).orElseThrow();
+        CsrfToken token = (CsrfToken) request.getAttribute("_csrf");
+        model.addAttribute("token", token.getToken());
+
+
+        model.addAttribute("name", userService.getUser(user.getId()).getName());
+        model.addAttribute("surname", userService.getUser(user.getId()).getSurname());
+        model.addAttribute("pass", userService.getUser(user.getId()).getPass());
+        model.addAttribute("email", userService.getUser(user.getId()).getEmail());
         return "modifyUser";
     }
 
-    @GetMapping("/user/{id}/userModified")
-    public String userModified(Model model,  @RequestParam String pass, @RequestParam String name, @RequestParam String surname,@PathVariable Long id){
+    @GetMapping("/user/userModified")
+    public String userModified(Model model, @RequestParam String pass, @RequestParam String name, @RequestParam String surname, HttpServletRequest request){
         /*userService.getUser(id).get().setName(name);
         userService.getUser(id).get().setSurname(surname);
         userService.getUser(id).get().setPass(pass);
          */
-        userService.modifyUser(id, new User(name, surname, pass, userService.getUser(id).getEmail()));
-        model.addAttribute("name", userService.getUser(id).getName());
+
+        String uda = request.getUserPrincipal().getName();
+        User user = userRepository.findByName(uda).orElseThrow();
+        CsrfToken token = (CsrfToken) request.getAttribute("_csrf");
+        model.addAttribute("token", token.getToken());
+
+        user.setName(name);
+        user.setSurname(surname);
+        user.setPass(pass);
+        userService.modifyUser(user.getId(), user);
+        model.addAttribute("name", userService.getUser(user.getId()).getName());
         return "userModified";
+
     }
 
 
@@ -109,74 +144,3 @@ public class UserController {
 }
 
 
-
-
-
-/*
-
---------------------------------------------
-
-
-
-
-
-
-
-
-
-@GetMapping("/showUsers")
-public String showUser(Model model){
-        model.addAttribute("users",userRepository.findAll());
-        return "usersTable";
-        }
-
-@GetMapping("/user/{id}")
-public String user(Model model, @PathVariable Long id){
-        model.addAttribute("name", userRepository.findById(id).get().getName());
-        return "userIndex";
-        }
-
-@GetMapping("/deleteUsers")
-public String deleteUsers(Model model){
-        model.addAttribute("users", userRepository.findAll());
-        return "deleteUsers";
-        }
-
-@GetMapping("/deleteUsers/{id}")
-public String deleteUser(Model model, @PathVariable Long id){
-        model.addAttribute("user", userRepository.findById(id).get().getName());
-        userRepository.deleteById(id);
-        return "userDeleted";
-        }
-@GetMapping("/addUsers")
-public String addUser(Model model){
-        model.addAttribute("users", userRepository.findAll());
-        return "addUsers";
-        }
-
-@GetMapping("/userAdded")
-public String formUser(Model model,  @RequestParam String pass, @RequestParam String name, @RequestParam String surname,@RequestParam String email){
-        userRepository.save(new User(name,surname,pass,email));
-        model.addAttribute("user",name);
-        return "userAdded";
-        }
-
-@GetMapping("/user/{id}/modifyUser")
-public String modifyUser(Model model, @PathVariable Long id){
-        model.addAttribute("name", userRepository.findById(id).get().getName());
-        model.addAttribute("surname", userRepository.findById(id).get().getSurname());
-        model.addAttribute("pass", userRepository.findById(id).get().getPass());
-        model.addAttribute("email", userRepository.findById(id).get().getEmail());
-        return "modifyUser";
-        }
-
-@GetMapping("/user/{id}/userModified")
-public String userModified(Model model,  @RequestParam String pass, @RequestParam String name, @RequestParam String surname,@PathVariable Long id){
-        userRepository.findById(id).get().setName(name);
-        userRepository.findById(id).get().setSurname(surname);
-        userRepository.findById(id).get().setPass(pass);
-        model.addAttribute("name", userRepository.findById(id).get().getName());
-        return "userModified";
-        }
-        }
-*/
